123b93f49ff96dd3c1176103d86331005416f561
Alan Patrick <alan.patrick.fv@gmail.com>
2022-06-12 19:07:36 -0300

    Set Card component

---
+++ src/component/Card.js
@@ -0,0 +1,10 @@
+import './Card.css';
+
+export default function Card(props) {
+  return (
+    <div className="card">
+      <img className='card-img' src="#" alt="Shows a landscape."/>
+      <p>I'm a caption</p>
+    </div>
+  );
+}
--- src/component/GameBoard.js
+++ src/component/GameBoard.js
@@ -1,9 +1,10 @@
+import Card from './Card';
 import './GameBoard.css';
 
 export default function GameBoard(props) {
   return (
     <div className="game-board">
-      {Array(12).fill(<p>hey, i'm a cell</p>)}
+      {Array(12).fill(<Card / >)}
     </div>
   );
 }
