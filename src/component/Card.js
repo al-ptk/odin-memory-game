5dec8b4fd914379b8390718e728d82e07fdd4942
Alan Patrick <alan.patrick.fv@gmail.com>
2022-06-12 22:55:33 -0300

    Set GameBoard to shuffle but broken

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import './Card.css';
 
 export default function Card(props) {
+  const { getScore, updateScore } = props.scoreState;
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (!clicked) {
+      setClicked(true);
+      updateScore(getScore() + 1)
+    } else {
+      updateScore(0)
+    }
+  };
+
   return (
-    <div className="card">
-      <img className="card-img" src={props.url} alt="Shows a landscape." />
+    <div className="card" onClick={handleClick}>
+      {props.img}
       <p>I'm a caption</p>
     </div>
   );
--- src/component/GameBoard.js
+++ src/component/GameBoard.js
@@ -4,22 +4,63 @@ import './GameBoard.css';
 
 export default function GameBoard(props) {
   const [images, setImages] = useState([]);
+  const [cards, setCards] = useState([]);
+  const [score, setScore] = useState(0);
 
+  // On mount, preload images
   useEffect(() => {
+    // Generate fetch promises for image urls
     let promises = [];
     for (let i = 0; i < 12; i++) {
       promises.push(fetch(`https://picsum.photos/200/300?random=${i}`));
     }
+    // Resolve promises into an array of components with loaded images
     Promise.all(promises)
       .then((data) => data.map((elem) => elem.url))
-      .then(imgArray => setImages(imgArray.map(elem => {
-        return <Card url={elem}/>
-      })))
+      .then((urlArray) =>
+        urlArray.map((url) => {
+          return <img className="card-img" src={url} alt="a pretty lanscape" />;
+        })
+      )
+      .then((imgComponents) => setImages(imgComponents));
   }, []);
 
-  if (!images.length) {
-    return <div>Loading</div>;
+  // On images loaded, build cards
+  useEffect(() => {
+    setCards(
+      images.map((img) => (
+        <Card
+          img={img}
+          scoreState={{
+            getScore: () => score,
+            updateScore: (newScore) => setScore(newScore),
+          }}
+        />
+      ))
+    );
+  }, [images]);
+
+  // On score change, shuffle cards
+  useEffect(() => {
+    setCards(getShuffledArray(cards));
+  }, [cards, score]);
+
+  // If images aren't loaded
+  if (!cards.length) {
+    return (
+      <div className="load-screen">
+        <p>Loading...</p>
+      </div>
+    );
   }
 
-  return <div className="game-board">{images}</div>;
+  return <div className="game-board">{cards}</div>;
+}
+
+function getShuffledArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
