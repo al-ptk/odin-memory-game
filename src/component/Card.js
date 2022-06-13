83adc282cc0dca68bd45fd7c7fcdb09b3e10ebdf
Alan Patrick <alan.patrick.fv@gmail.com>
2022-06-12 21:28:17 -0300

    Set GameBoard to preload images

--- src/component/Card.js
+++ src/component/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 export default function Card(props) {
   return (
     <div className="card">
-      <img className='card-img' src="#" alt="Shows a landscape."/>
+      <img className="card-img" src={props.url} alt="Shows a landscape." />
       <p>I'm a caption</p>
     </div>
   );
--- src/component/GameBoard.js
+++ src/component/GameBoard.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
 import Card from './Card';
 import './GameBoard.css';
 
 export default function GameBoard(props) {
-  return (
-    <div className="game-board">
-      {Array(12).fill(<Card / >)}
-    </div>
-  );
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    let promises = [];
+    for (let i = 0; i < 12; i++) {
+      promises.push(fetch(`https://picsum.photos/200/300?random=${i}`));
+    }
+    Promise.all(promises)
+      .then((data) => data.map((elem) => elem.url))
+      .then(imgArray => setImages(imgArray.map(elem => {
+        return <Card url={elem}/>
+      })))
+  }, []);
+
+  if (!images.length) {
+    return <div>Loading</div>;
+  }
+
+  return <div className="game-board">{images}</div>;
 }
