db9ead7ebbdf4f25041b5ab968ad3af42d5c5a52
Alan Patrick <alan.patrick.fv@gmail.com>
2022-08-01 09:06:15 -0300

    Fix small warning

--- src/component/GameBoard.js
+++ src/component/GameBoard.js
@@ -1,4 +1,4 @@
-import { Component, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Card from './Card';
 import './GameBoard.css';
 import { getImages, getCaptions } from '../fetchYeMemories';
